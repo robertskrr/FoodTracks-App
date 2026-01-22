f832cbc6a08f6f6fdad1d86772cd2c52e3a66b34
Robert Esquerre <esquerre.valiente.robert.ernesto@iescamas.es>
2026-01-22 20:00:55 +0100

    conexion firebase

--- FoodTracks/app/src/main/java/com/example/foodtracks/MainActivity.java
+++ FoodTracks/app/src/main/java/com/example/foodtracks/MainActivity.java
@@ -5,13 +5,20 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseFirestore mFirestore; // TRABAJARA CON LOS DATOS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(getApplicationContext(), "HOLAAA", Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "HOLAAA BUENAS TARDES", Toast.LENGTH_SHORT).show();
+    }
+
+    private void asignarComponentes() {
+        mFirestore = FirebaseFirestore.getInstance();
     }
 
 
