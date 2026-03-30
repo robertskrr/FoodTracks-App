00f96c069e30067ee281a91b963b4cf5c29f2224
Robert Esquerre Valiente <robertesquerre24@gmail.com>
2026-03-30 13:24:20 +0200

    feat: Added exceptions for users (validation and not found)

---
+++ FoodTracksApp/app/src/main/java/com/foodtracks/app/services/exceptions/UsuarioValidationException.java
@@ -0,0 +1,18 @@
+package com.foodtracks.app.services.exceptions;
+
+import lombok.Getter;
+
+/**
+ * Gestiona las validaciones en cuanto al usuario
+ * @author Robert
+ * @since 30/03
+ */
+@Getter
+public class UsuarioValidationException extends Exception{
+
+    private final int resId;
+    public UsuarioValidationException(int resId) {
+        this.resId = resId;
+    }
+
+}
